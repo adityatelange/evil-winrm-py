190713c4d5a8ffe477d38ea71640822619f3b0ad
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2025-04-14 01:02:23 +0530

    Normalize help text for command-line arguments in main function

--- evil_winrm_py/evil_winrm_py.py
+++ evil_winrm_py/evil_winrm_py.py
@@ -158,15 +158,15 @@ def main():
         "-i",
         "--ip",
         required=True,
-        help="Remote host IP or hostname",
+        help="remote host IP or hostname",
     )
-    parser.add_argument("-u", "--user", required=True, help="Username")
-    parser.add_argument("-p", "--password", help="Password")
+    parser.add_argument("-u", "--user", required=True, help="username")
+    parser.add_argument("-p", "--password", help="password")
     parser.add_argument(
-        "--port", type=int, default=5985, help="Remote host port (default 5985)"
+        "--port", type=int, default=5985, help="remote host port (default 5985)"
     )
     parser.add_argument(
-        "--version", action="version", version=__version__, help="Show version"
+        "--version", action="version", version=__version__, help="show version"
     )
 
     args = parser.parse_args()
